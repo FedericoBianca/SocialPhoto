54df88887296cb8e6f8a7b3a7456cd4bab14c9e5
Federico <federico.bianca95@gmail.com>
2018-09-10 17:19:24 +0200

    Added FrontEnd and BackEnd

--- FrontEnd/app/src/main/java/com/example/social_photo/OverviewTab.java
+++ FrontEnd/app/src/main/java/com/example/social_photo/OverviewTab.java
@@ -5,11 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class OverviewTab extends Fragment {
+    private ListView listView;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.overview_tab, container, false);
+        listView = rootView.findViewById(R.id.listView);
+
+
         return rootView;
     }
 }
